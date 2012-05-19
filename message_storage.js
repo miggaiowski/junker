27811db7d7b58e3087aee228600941b8f751d044
Thiago Valverde <thiago@valverde.me>
2012-05-19 08:50:53 -0300

    Storage fixes

--- message_storage.js
+++ message_storage.js
@@ -2,7 +2,7 @@ chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
   if(request.method == "get"){
     
     if(!localStorage.perf)
-      sendResponse( { userData: null} );
+      sendResponse( { userData: null } );
     else
       sendResponse( { userData: JSON.parse(localStorage.perf) });
     
